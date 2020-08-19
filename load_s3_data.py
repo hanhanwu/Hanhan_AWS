2249bd81a4dfbc96630e666e88f2f67827fc2e2f
hanhan <hanhanwu@users.noreply.github.com>
2020-08-19 16:53:10 -0700

    Update load_s3_data.py

--- load_s3_data.py
+++ load_s3_data.py
@@ -1,6 +1,5 @@
-# To load S3 csv
+# To load S3 csv, need to do `pip install s3fs`
 import pandas as pd
-import s3fs
 
 # Somtimes you don't know the delimiter, put `error_bad_lines=False` in `read_csv()` function might help
 df = pd.read_csv('s3://[file_path]',  # get the file path from S3
